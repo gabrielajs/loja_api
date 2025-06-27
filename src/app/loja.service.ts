0ba7b2d183439ba169425fb0b2718451d309d69b
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-06-26 21:53:21 -0300

    criando loja.service para consumir a API

---
+++ src/app/loja.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LojaService {
+  //url da api
+  private apiUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http:HttpClient) { }
+}
