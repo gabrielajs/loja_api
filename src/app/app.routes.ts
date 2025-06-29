178d80dd0a8818f9eeee017c9ef0706dac9a2398
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-06-28 23:21:03 -0300

    criando designer da pagina de produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    
+    {
+        //rota de redirecionamento de produtos
+        path: 'produtos',
+        loadComponent: () => import('./pages/produtos/produtos').then(m => m.Produtos)
+    },
     {
         //inicio da pagina mostra os produtos
         path: '',
         redirectTo: 'produtos',
         pathMatch: 'full'
-    },
-    {
-        //rota de redirecionamento de produtos
-        path: 'produtos',
-        loadComponent: () => import('./pages/produtos/produtos').then(m => m.Produtos)
     }
 ];
 
