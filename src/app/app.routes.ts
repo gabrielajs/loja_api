0cbf0b42538dd9c0b9f9e0cd9b40dd5a19f4c7b4
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-06-28 23:17:58 -0300

    criando component produtos e rotas para a pagina de produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        //inicio da pagina mostra os produtos
+        path: '',
+        redirectTo: 'produtos',
+        pathMatch: 'full'
+    },
+    {
+        //rota de redirecionamento de produtos
+        path: 'produtos',
+        loadComponent: () => import('./pages/produtos/produtos').then(m => m.Produtos)
+    }
+];
+
+ 
