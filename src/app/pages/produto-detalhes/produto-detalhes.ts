0e238d02f8f417223484d03a6423b5e4b09cbc9f
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-06-30 21:35:35 -0300

    criando metodo que obtem o id do produto atraves da rota, criando a pagina de detalhes de produto e configurando para pegar as informações do produto e exibir na tela

--- src/app/pages/produto-detalhes/produto-detalhes.ts
+++ src/app/pages/produto-detalhes/produto-detalhes.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { LojaService } from '../../loja.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produto-detalhes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produto-detalhes.html',
   styleUrl: './produto-detalhes.css'
 })
 export class ProdutoDetalhes {
+
+  produto: any = {};
+
+  constructor(private lojaService: LojaService,
+              private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(){
+    this.getProdutoId();
+  }
+
+  //retornando produto por id
+  getProdutoId(){
+    // obtem o id do produto da rota
+    this.route.paramMap.subscribe(params => {
+      const id = Number(params.get('id'));
+      if (id) {
+        this.lojaService.getProdutoById(id).subscribe((data: any) => {
+          this.produto = data;
+          console.log(this.produto);
+        });
+      }
+    });
+  }
 
 }
