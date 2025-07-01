00a597c0b5dc73e8e23f7d973cefe4949d1d0590
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-06-30 21:51:56 -0300

    inserindo link no menu product e também dos produtos ao clicar ir para pagina de detalhamento do produto

--- src/app/pages/produtos/produtos.ts
+++ src/app/pages/produtos/produtos.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LojaService } from '../../loja.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './produtos.html',
   styleUrl: './produtos.css'
 })
